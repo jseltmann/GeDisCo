f875f120905a819027b5a6e07bfe4e4bac3f75d1
Johann Seltmann <jseltmann@uni-potsdam.de>
2020-04-01 10:13:04 +0000

    prepare for translation

--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import xml.etree.ElementTree as ET
+import re
 
 def get_europarl_overlap(xml_path, langs):
     """
@@ -80,17 +81,62 @@ def xml_to_txt(xml_path, txt_path):
 
     for fn in os.listdir(xml_path):
         file_path = os.path.join(xml_path,fn)
-        tree = ET.parse(file_path)
-        sents = tree.iter(tag='s')
-        sents = [s.iter(tag='w') for s in sents]
-        sents = [" ".join(word.text) for s in sents for word in s]
+        try:
+            tree = ET.parse(file_path)
+        except Exception as e:
+            print(fn)
+            raise e
+        speakers = tree.iter(tag="SPEAKER")
+        president_pattern = r"[P|p]räsident|[P|p]resident|[p|P]ředsed|[p|P]résiden"
+        for i, speaker in enumerate(speakers):
+            if re.search(president_pattern,speaker.attrib['NAME']):
+                # exclude things said by the president, since these are not part of the debate
+                continue
+            sents = list(speaker.iter(tag='s'))
+            sent_inds = [s.attrib['id'] for s in sents]
+            sents = [s.iter(tag='w') for s in sents]
+            sents = [[word.text for word in s] for s in sents]
+            sents = [" ".join(s) for s in sents]
 
-        plain_path = os.path.join(txt_path, fn)
-        with open(plain_path, "w") as plain_file:
-            for sent in sents:
-                plain_file.write(sent)
-                plain_file.write("\n")
+            text_fn = fn[:-4] + "_" + str(i) + ".txt"
+            plain_path = os.path.join(txt_path, text_fn)
+            with open(plain_path, "w") as plain_file:
+                for sent in sents:
+                    plain_file.write(sent)
+                    plain_file.write("\n")
+            inds_fn = fn[:-4] + "_" + str(i) + ".inds"
+            inds_path = os.path.join(txt_path, inds_fn)
+            with open(inds_path, "w") as inds_file:
+                for ind in sent_inds:
+                    inds_file.write(ind + "\n")
 
 
-xml_to_txt("/home/users/jseltmann/data/europarl/common/xml/en", 
-           "/home/users/jseltmann/data/europarl/common/txt/en")
+for lang in ["en", "de", "fr", "cs"]:
+    xml_to_txt("/home/users/jseltmann/data/europarl/common/xml/" + lang, 
+               "/home/users/jseltmann/data/europarl/common/txt/" + lang)
+
+
+def append_files(txt_dir, combined_path):
+    """
+    Append txt files into one combined file for better processing by moses.
+    Delineate files with lines of "##########" and filenames, so that they
+    can be split again.
+
+    Parameters
+    ----------
+    txt_dir : str
+        Directory containing text files to be appended.
+    combined_path : str
+        Path to save the combined file to.
+    """
+
+    with open(combined_path, "w") as comb_file:
+        for fn in os.listdir(txt_dir):
+            file_path = os.path.join(txt_dir, fn)
+            comb_file.write("##############################" + fn + "\n")
+            with open(file_path) as txt_file:
+                for line in txt_file:
+                    comb_file.write(line)
+
+
+#append_files("/home/users/jseltmann/data/europarl/common/txt/fr", "/home/users/jseltmann/data/europarl/common/comb/fr.txt")
