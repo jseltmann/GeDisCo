e3641aef3b36c9cfdb9a06bef163978f3157f259
Johann Seltmann <johann.seltmann@posteo.net>
2020-08-25 13:13:50 +0000

    changed analysis to work on transferred files, rather than pcc xml

--- analysis.py
+++ analysis.py
@@ -48,30 +48,23 @@ def analyze_dir(to_analyze, txt_dir, res_path):
         num_texts += 1
         num_words += word_count
 
-        xml_path = os.path.join(to_analyze, fn)
-        tree = etree.parse(xml_path)
-        relations = tree.findall(".//relation")
         empt_in_doc = False
-        for relation in relations:
+        json_path = os.path.join(to_analyze, fn)
+        with open(json_path) as json_file:
+            lines = json_file.readlines()
+        for line in lines:
+            relation = json.loads(line)
             num_relations += 1
-            if relation.attrib["type"] == "explicit":
-                num_explicit += 1
-            elif relation.attrib["type"] == "implicit":
-                num_implicit += 1
-            else:
-                num_other_types += 1
-            
-            if relation.attrib["pdtb3_sense"] in sense_counts:
-                sense_counts["sense"] += 1
-            else:
-                sense_counts["sense"] = 1
 
-            ct = relation.find("connective_tokens")
-            c_inds = set([t.attrib["id"] for t in ct])
-            it = relation.find("int_arg_tokens")
-            i_inds = set([t.attrib["id"] for t in it])
-            et = relation.find("ext_arg_tokens")
-            e_inds = set([t.attrib["id"] for t in et])
+            sense = relation["Sense"][0]
+            if sense in sense_counts:
+                sense_counts[sense] += 1
+            else:
+                sense_counts[sense] = 1
+
+            c_inds = set(relation["Connective"]["TokenList"])
+            i_inds = set(relation["Arg1"]["TokenList"])
+            e_inds = set(relation["Arg2"]["TokenList"])
 
             if len(e_inds) == 0 or len(i_inds) == 0:
                 rel_empt_arg += 1
@@ -82,7 +75,8 @@ def analyze_dir(to_analyze, txt_dir, res_path):
                     s1.add(el)
                 return s1
 
-            if relation.attrib["type"] == "implicit":
+            if relation["Type"] == "Implicit":
+                num_implicit += 1
                 if i_inds in sents and e_inds in sents:
                     both_sents += 1
                 elif set_add(i_inds, c_inds) in sents and e_inds in sents:
@@ -95,15 +89,17 @@ def analyze_dir(to_analyze, txt_dir, res_path):
                         one_sent += 1
                     elif e_inds in sents or set_add(e_inds, c_inds) in sents:
                         one_sent += 1
-            else:
+            elif relation["Type"] == "Explicit":
+                num_explicit += 1
                 arg_inds = set_add(i_inds, e_inds)
                 if c_inds.issubset(arg_inds):
                     conn_in_arg += 1
+            else:
+                num_other_types += 1
 
         if empt_in_doc:
-            rels_doc_with_empt += len(relations)
+            rels_doc_with_empt += len(lines)
             doc_empt_arg += 1
-
 
     with open(res_path, "w") as res_file:
         res_file.write("analyzed directory: " + to_analyze + "\n")
@@ -168,15 +164,211 @@ def analyze_dir(to_analyze, txt_dir, res_path):
         res_file.write("#docs with empty args: " + str(doc_empt_arg) + "\n")
         frac_of_docs = str(float(doc_empt_arg) / float(num_texts))
         res_file.write("fraction: " + frac_of_docs  + "\n")
-        frac_empt_in_empt_docs = str(float(rel_empt_arg) /
-                                     float(rels_doc_with_empt))
+        if rels_doc_with_empt > 0:
+            frac_empt_in_empt_docs = str(float(rel_empt_arg) /
+                                         float(rels_doc_with_empt))
+        else:
+            frac_empt_in_empt_docs = ""
         res_file.write("fraction of empty among rels in docs with empty: " + 
                 frac_empt_in_empt_docs + "\n")
 
 
-#analyze_dir("/data/europarl/common/pcc_labels/from_en",
-#            "/data/europarl/common/txt/de",
-#            "/data/europarl/common/analysis/from_en.txt")
+analyze_dir("/data/europarl/common/transferred/from_en",
+            "/data/europarl/common/txt/de",
+            "/data/europarl/common/analysis/corpora/from_en.txt")
+analyze_dir("/data/europarl/common/transferred/from_cs",
+            "/data/europarl/common/txt/de",
+            "/data/europarl/common/analysis/corpora/from_cs.txt")
+analyze_dir("/data/europarl/common/transferred/from_fr",
+            "/data/europarl/common/txt/de",
+            "/data/europarl/common/analysis/corpora/from_en.txt")
+analyze_dir("/data/europarl/common/parsed/en_cs",
+            "/data/europarl/common/txt/de",
+            "/data/europarl/common/analysis/corpora/en_cs.txt")
+analyze_dir("/data/europarl/common/parsed/en_fr",
+            "/data/europarl/common/txt/de",
+            "/data/europarl/common/analysis/corpora/en_fr.txt")
+analyze_dir("/data/europarl/common/parsed/cs_fr",
+            "/data/europarl/common/txt/de",
+            "/data/europarl/common/analysis/corpora/cs_fr.txt")
+analyze_dir("/data/europarl/common/parsed/cs_fr_en",
+            "/data/europarl/common/txt/de",
+            "/data/europarl/common/analysis/corpora/cs_fr_en.txt")
+
+
+def analyze_dir_pcc(to_analyze, txt_dir, res_path):
+    """
+    Analyze dataset in one directory, but work on PCC xml files.
+
+    Parameters
+    ----------
+    to_analyze : str
+        Path to directory for which to do the analysis.
+    txt_dir : str
+        Path to directory containing text files.
+    res_path : str
+        Path to write results to.
+    """
+
+    sense_counts = dict()
+    num_relations = 0
+    num_texts = 0
+    num_explicit = 0
+    num_implicit = 0
+    num_other_types = 0
+    one_sent = 0
+    both_sents = 0
+    conn_in_arg = 0
+    num_words = 0
+    doc_empt_arg = 0
+    rel_empt_arg = 0
+    rels_doc_with_empt = 0
+
+    for fn in tqdm(os.listdir(to_analyze)):
+        txt_path = os.path.join(txt_dir, fn.split(".")[0] + ".txt")
+        with open(txt_path) as txt_file:
+            lines = txt_file.readlines()
+            sents = [line.split() for line in lines]
+            sent_inds = []
+            word_count = 0
+            for sent in sents:
+                inds = set([i + word_count for i in range(len(sent))])
+                sent_inds.append(inds)
+                word_count += len(sent)
+
+        num_texts += 1
+        num_words += word_count
+
+        xml_path = os.path.join(to_analyze, fn)
+        tree = etree.parse(xml_path)
+        relations = tree.findall(".//relation")
+        empt_in_doc = False
+        for relation in relations:
+            num_relations += 1
+
+            sense = relation.attrib["pdtb3_sense"]
+            if sense in sense_counts:
+                sense_counts[sense] += 1
+            else:
+                sense_counts[sense] = 1
+
+            ct = relation.find("connective_tokens")
+            c_inds = set([t.attrib["id"] for t in ct])
+            it = relation.find("int_arg_tokens")
+            i_inds = set([t.attrib["id"] for t in it])
+            et = relation.find("ext_arg_tokens")
+            e_inds = set([t.attrib["id"] for t in et])
+
+            if len(e_inds) == 0 or len(i_inds) == 0:
+                rel_empt_arg += 1
+                empt_in_doc = True
+
+            def set_add(s1, s2):
+                for el in s2:
+                    s1.add(el)
+                return s1
+
+            if relation.attrib["type"] == "implicit":
+                num_implicit += 1
+                if i_inds in sents and e_inds in sents:
+                    both_sents += 1
+                elif set_add(i_inds, c_inds) in sents and e_inds in sents:
+                    both_sents += 1
+                elif i_inds in sents and set_add(e_inds, c_inds) in sents:
+                    both_sents += 1
+                else:
+                    # one of the arguments could still be a sentence
+                    if i_inds in sents or set_add(i_inds, c_inds) in sents:
+                        one_sent += 1
+                    elif e_inds in sents or set_add(e_inds, c_inds) in sents:
+                        one_sent += 1
+            elif relation.attrib["type"] == "explicit":
+                num_explicit += 1
+                arg_inds = set_add(i_inds, e_inds)
+                if c_inds.issubset(arg_inds):
+                    conn_in_arg += 1
+            else:
+                num_other_types += 1
+
+        if empt_in_doc:
+            rels_doc_with_empt += len(lines)
+            doc_empt_arg += 1
+
+    with open(res_path, "w") as res_file:
+        res_file.write("analyzed directory: " + to_analyze + "\n")
+        res_file.write("number of documents: " + str(num_texts) + "\n")
+        res_file.write("number of words: " + str(num_words) + "\n")
+        res_file.write("number of relations: "+ str(num_relations) + "\n")
+        if num_texts > 0:
+            frac_docs = float(num_relations) / float(num_texts)
+            res_file.write("relations per document: " + str(frac_docs) + "\n")
+        else:
+            res_file.write("relations per document: \n")
+        if num_words > 0:
+            frac_words = float(num_relations) / float(num_words)
+            res_file.write("relations per word: " + str(frac_words) + "\n")
+        else:
+            res_file.write("relations per word: \n")
+
+        res_file.write("\n#### Senses ####\n")
+        for sense in sense_counts:
+            if num_relations > 0:
+                frac = float(sense_counts[sense]) / float(num_relations)
+                res_file.write(sense + ": " + str(frac) + "\n")
+            else:
+                res_file.write(sense + ": \n")
+        res_file.write("\n")
+        if num_relations > 0:
+            frac_impl = float(num_implicit) / float(num_relations)
+            res_file.write("fraction implicit: " + str(frac_impl) + "\n")
+            frac_expl = float(num_explicit) / float(num_relations)
+            res_file.write("fraction explicit: " + str(frac_expl) + "\n")
+        else:
+            res_file.write("fraction implicit: \n")
+            res_file.write("fraction explicit: \n")
+
+        res_file.write("\n#### Arguments ####\n")
+        if num_implicit > 0:
+            frac_one_sent = float(one_sent) / float(num_implicit)
+            res_file.write("Fraction of implicit where one argument ")
+            res_file.write("is a sentence: " + str(frac_one_sent) + "\n")
+            frac_both_sent = float(both_sents) / float(num_implicit)
+            res_file.write("Fraction of implicit where both arguments ")
+            res_file.write("are sentences: " + str(frac_both_sent) + "\n")
+        else:
+            res_file.write("Fraction of implicit where one argument ")
+            res_file.write("is a sentence: \n")
+            res_file.write("Fraction of implicit where both arguments ")
+            res_file.write("are sentences: \n")
+        
+        if num_explicit > 0:
+            frac_conn_in_arg = float(conn_in_arg) / float(num_explicit)
+            res_file.write("\nFraction of explicit where the connective")
+            res_file.write(" is also part of one or both arguments: ")
+            res_file.write(str(frac_conn_in_arg) + "\n")
+        else:
+            res_file.write("\nFraction of explicit where the connective")
+            res_file.write(" is also part of one or both arguments: \n")
+
+        res_file.write("\n#####Relations with empty arguments####\n")
+        res_file.write("number: " + str(rel_empt_arg) + "\n")
+        frac_of_rels = str(float(rel_empt_arg) / float(num_relations))
+        res_file.write("fraction: " + frac_of_rels  + "\n")
+        res_file.write("#docs with empty args: " + str(doc_empt_arg) + "\n")
+        frac_of_docs = str(float(doc_empt_arg) / float(num_texts))
+        res_file.write("fraction: " + frac_of_docs  + "\n")
+        if rels_doc_with_empt > 0:
+            frac_empt_in_empt_docs = str(float(rel_empt_arg) /
+                                         float(rels_doc_with_empt))
+        else:
+            frac_empt_in_empt_docs = ""
+        res_file.write("fraction of empty among rels in docs with empty: " + 
+                frac_empt_in_empt_docs + "\n")
+
+
+analyze_dir("/data/PotsdamCommentaryCorpus/connectives",
+            "/data/PotsdamCommentaryCorpus/tokenized",
+            "/data/europarl/common/analysis/corpora/pcc.txt")
 
 
 def analyze_transfer(orig_dir, trans_dir, out_path):
@@ -300,13 +492,12 @@ def analyze_transfer(orig_dir, trans_dir, out_path):
         out_file.write(outs)
 
 
-#analyze_transfer(orig_dir, trans_dir, out_path):
-analyze_transfer("/data/europarl/common/parsed/en",
-                 "/data/europarl/common/transferred/from_en",
-                 "/data/europarl/common/analysis/transfer/en")
-analyze_transfer("/data/europarl/common/parsed/fr",
-                 "/data/europarl/common/transferred/from_fr",
-                 "/data/europarl/common/analysis/transfer/fr")
-analyze_transfer("/data/europarl/common/parsed/cs",
-                 "/data/europarl/common/transferred/from_cs",
-                 "/data/europarl/common/analysis/transfer/cs")
+#analyze_transfer("/data/europarl/common/parsed/en",
+#                 "/data/europarl/common/transferred/from_en",
+#                 "/data/europarl/common/analysis/transfer/en")
+#analyze_transfer("/data/europarl/common/parsed/fr",
+#                 "/data/europarl/common/transferred/from_fr",
+#                 "/data/europarl/common/analysis/transfer/fr")
+#analyze_transfer("/data/europarl/common/parsed/cs",
+#                 "/data/europarl/common/transferred/from_cs",
+#                 "/data/europarl/common/analysis/transfer/cs")
